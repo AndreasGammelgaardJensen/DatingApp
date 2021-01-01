d78925695f63ff6f9b635ab47b1e8847e3ad6988
AndreasBGJensen <bgandreas@hotmail.com>
2021-01-01 01:14:10 +0100

    ok

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName, Password)
+VALUES (1,"Andreas","1234");
+
+INSERT INTO Users (Id, UserName, Password)
+VALUES (2,"Per","12");
+
+INSERT INTO Users (Id, UserName, Password)
+VALUES (3,"Claus","123");
